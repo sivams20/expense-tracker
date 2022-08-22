22aa19f9d79c25ed97d015e331708d56ba3fb95f
unknown <sivams20@gmail.com>
2022-08-22 22:46:08 +0530

    Fixed the routing

--- src/components/Spending/Spending.js
+++ src/components/Spending/Spending.js
@@ -6,11 +6,11 @@ import { Formik, useFormik } from 'formik';
 import * as Yup from 'yup';
 
 const SpendingContainer = styled.div`
-    position: fixed;
+    /* position: fixed;
     top: 0;
     left: 0;
     right: 0;
-    bottom: 0;
+    bottom: 0; */
 
     display: flex;
     justify-content: center;
