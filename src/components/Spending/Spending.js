b6da0638532b3cd11c8a1039372a7e17f27d8a96
unknown <sivams20@gmail.com>
2022-11-17 12:33:27 +0530

    form rewamp

--- src/components/Spending/Spending.js
+++ src/components/Spending/Spending.js
@@ -2,17 +2,11 @@ import React, { useState } from "react";
 import styled from 'styled-components';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { Formik, Field } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { useSelector } from "react-redux";
 
 const SpendingContainer = styled.div`
-    /* position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0; */
-
     display: flex;
     justify-content: center;
     align-items: center;
@@ -26,39 +20,50 @@ const SpendingForm = styled.form`
 `
 
     const initialValues = {
-      price: '0',
-      category: '',
-      date: new Date()
+        date: new Date(),
+        amount: '0',
+        category: '',
+        note: ''
     };
 
     const validationSchema = Yup.object({
-        price: Yup.string().required('Required'),
-        category: Yup.string().required('Required'),
         date: Yup.string().required('Required'),
+        amount: Yup.string().required('Required'),
+        category: Yup.string().required('Required'),
+        note: Yup.string().required('Required')
     })
-    
+
     const onSubmit = values => {
         console.log(values);
     }
 
 const Spending = ({ ...props }) => {
-    const categories = useSelector(state => state.category.categories);
+    //const categories = useSelector(state => state.category.categories);
     return(
         <Formik 
             initialValues={initialValues}
             validationSchema={validationSchema}
             onSubmit={onSubmit}>
-            <SpendingContainer>
-                <SpendingForm>
-                    <Field type="text" placeholder="price" id="price" name="price"/>
-                    {/* <Field id="category" name="category">
-                        {categories.map((option) => (
-                        <option key={option._id} value={option.name}>{option.name}</option>
-                        ))}
-                    </Field> */}
+            {/* <SpendingContainer> */}
+                <Form>
+                    <div>
+                        <label htmlFor="">Amount</label>
+                        <Field type="text" id="amount" name="amount" />
+                        <ErrorMessage name="amount" />
+                    </div>
+                    <div>
+                        <label htmlFor="">Category</label>
+                        <Field type="text" id="category" name="category" />
+                        <ErrorMessage name="category" />
+                    </div>
+                    <div>
+                        <label htmlFor="">Note</label>
+                        <Field type="text" id="note" name="note" />
+                        <ErrorMessage name="note" />
+                    </div>
                     <button>Add Expense</button>
-                </SpendingForm>
-            </SpendingContainer>
+                </Form>
+            {/* </SpendingContainer> */}
         </Formik>
     )
     
