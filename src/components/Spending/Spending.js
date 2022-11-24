2631e08139175bb98feeb84f3570960c130920ba
unknown <sivams20@gmail.com>
2022-11-24 09:24:04 +0530

    Added datepicker

--- src/components/Spending/Spending.js
+++ src/components/Spending/Spending.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import styled from 'styled-components';
+import DateView from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
@@ -49,41 +50,59 @@ const Spending = ({ ...props }) => {
 
     console.log(categories);
     return(
-                    <Formik 
-                        initialValues={initialValues}
-                        validationSchema={validationSchema}
-                        onSubmit={onSubmit}>
-                        {/* <SpendingContainer> */}
-                            <Form>
-                                <div className="form-control">
-                                    <label htmlFor="amount">Amount</label>
-                                    <Field type="text" id="amount" name="amount" />
-                                    <ErrorMessage name="amount" component={TextError} />
-                                </div>
-                                <div className="form-control">
-                                    <label htmlFor="category">Category</label>
-                                    <Field as="select" id="category" name="category">
-                                        {categories.map(category => {
-                                            return(
-                                                <option key={category._id} value={category.id}>
-                                                    {category.name}
-                                                </option>
-                                            )
-                                        })}
-                                    </Field>
-                                    <ErrorMessage name="category" component={TextError} />
-                                </div>
-                                <div className="form-control">
-                                    <label htmlFor="note">Note</label>
-                                    <Field type="text" id="note" name="note" />
-                                    <ErrorMessage name="note">
-                                        {errorMssg => <div className="error">{errorMssg}</div>}
-                                    </ErrorMessage>
-                                </div>
-                                <button>Add Expense</button>
-                            </Form>
-                        {/* </SpendingContainer> */}
-                    </Formik>
+        <Formik 
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}>
+            {/* <SpendingContainer> */}
+                <Form>
+                    <div className="form-control">
+                        <label htmlFor="amount">Amount</label>
+                        <Field type="text" id="amount" name="amount" />
+                        <ErrorMessage name="amount" component={TextError} />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="category">Category</label>
+                        <Field as="select" id="category" name="category">
+                            {categories.map(category => {
+                                return(
+                                    <option key={category._id} value={category.id}>
+                                        {category.name}
+                                    </option>
+                                )
+                            })}
+                        </Field>
+                        <ErrorMessage name="category" component={TextError} />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="note">Note</label>
+                        <Field type="text" id="note" name="note" />
+                        <ErrorMessage name="note">
+                            {errorMssg => <div className="error">{errorMssg}</div>}
+                        </ErrorMessage>
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor="date">Date</label>
+                        <Field name="date">
+                            {({ form, field }) => {
+                            const { setFieldValue } = form
+                            const { value } = field
+                            return (
+                                <DateView
+                                id="date"
+                                selected={value}
+                                onChange={val => setFieldValue('date', val)}
+                                dateFormat="dd/MM/yyyy"
+                                />
+                            )
+                            }}
+                        </Field>
+                        <ErrorMessage component={TextError} name="date" />
+                    </div>
+                    <button>Add Expense</button>
+                </Form>
+            {/* </SpendingContainer> */}
+        </Formik>
     )
     
 }
