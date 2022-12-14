a4fc8e966386869604f4df9b32888dcddf7531d2
unknown <sivams20@gmail.com>
2022-12-14 09:43:30 +0530

    Added constants for menu build

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { menuList } from "../../util/constants";
 import "./Navbar.css";
 
 function Navbar() {
@@ -7,24 +8,14 @@ function Navbar() {
     <div className="nav-container">
       <nav className="nav-menu active">
         <ul className="nav-menu-items">
-          <li className="nav-text">
-            <Link to="/spending">
-              {/* {item.icon} */}
-              <span>spending</span>
-            </Link>
-          </li>
-          <li className="nav-text">
-            <Link to="/transaction">
-              {/* {item.icon} */}
-              <span>transaction</span>
-            </Link>
-          </li>
-          <li className="nav-text">
-            <Link to="/category">
-              {/* {item.icon} */}
-              <span>category</span>
-            </Link>
-          </li>
+          {menuList.map((menu) => (
+            <li className="nav-text">
+              <Link to={menu.link}>
+                {/* {item.icon} */}
+                <span>{menu.name}</span>
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </div>
---
+++ src/util/constants.js
@@ -0,0 +1,5 @@
+export const menuList = [
+  { name: "Spending", link: "/spending", icon: "" },
+  { name: "Transaction", link: "/transaction", icon: "" },
+  { name: "Category", link: "/category", icon: "" },
+];
