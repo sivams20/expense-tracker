cd76b2f33415153649a4c15a344547282e579c55
unknown <sivams20@gmail.com>
2023-01-03 09:25:55 +0530

    Changed routing strategy to hashRouting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { HashRouter, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import Category from "./components/Category/Category";
 import Error from "./components/Error/Error";
@@ -17,13 +17,15 @@ function App() {
       <div className="App">
         {pathname === "/login" ? null : <Navbar />}
         <div className="container">
-          <Routes>
-            <Route path="login" element={<Login />} />
-            <Route path="spending" element={<Spending />} />
-            <Route path="transaction" element={<Transaction />} />
-            <Route path="category" element={<Category />} />
-            <Route path="*" element={<Error />} />
-          </Routes>
+          <HashRouter>
+            <Routes>
+              <Route path="login" element={<Login />} />
+              <Route path="spending" element={<Spending />} />
+              <Route path="transaction" element={<Transaction />} />
+              <Route path="category" element={<Category />} />
+              <Route path="*" element={<Error />} />
+            </Routes>
+          </HashRouter>
         </div>
       </div>
     </Provider>
