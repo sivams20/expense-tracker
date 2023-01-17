d9d0e6a2421bb16f3bd89d6a9e071b1f49d2c8e3
unknown <sivams20@gmail.com>
2023-01-17 08:48:32 +0530

    Fetching spendings from store

--- src/components/Transaction/Transaction.js
+++ src/components/Transaction/Transaction.js
@@ -1,17 +1,34 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSpending } from "../../redux/spending/spendingActions";
+import styled from "styled-components";
+
+const TransactionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 function Transaction() {
   const dispatch = useDispatch();
-  const spendings = useSelector((state) => state);
+  const spendings = useSelector((state) => state.spending.spendings);
   console.log(spendings);
 
   useEffect(() => {
     dispatch(fetchSpending());
   }, [dispatch]);
 
-  return <div>Transaction</div>;
+  return (
+    <TransactionContainer>
+      {spendings.length > 0 &&
+        spendings.map((item) => (
+          <div>
+            {item.note}, {item.date}, {item.amount}
+          </div>
+        ))}
+    </TransactionContainer>
+  );
 }
 
 export default Transaction;
