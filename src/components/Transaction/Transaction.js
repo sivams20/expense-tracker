f646a90726dd725a35669bb58e7e66aa9fae73c2
unknown <sivams20@gmail.com>
2023-01-25 08:39:28 +0530

    Transaction list style added

--- src/components/Transaction/Transaction.js
+++ src/components/Transaction/Transaction.js
@@ -8,6 +8,24 @@ const TransactionContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  width: 700px;
+`;
+
+const TransactionItem = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  border-radius: 5px;
+  padding: 15px 5px;
+  margin-bottom: 5px;
+`;
+
+const Category = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
 function Transaction() {
@@ -23,9 +41,13 @@ function Transaction() {
     <TransactionContainer>
       {spendings.length > 0 &&
         spendings.map((item) => (
-          <div>
-            {item.note}, {item.date}, {item.amount}
-          </div>
+          <TransactionItem>
+            <Category>
+              <span>{item.note}</span>
+              <span>{item.date}</span>
+            </Category>
+            <div>{item.amount}</div>
+          </TransactionItem>
         ))}
     </TransactionContainer>
   );
